190e1f59f32a53c6f5fae2ce9cde04d1b316121e
5619akshadhareddy-boop <5619akshadhareddy@gmail.com>
2026-07-31 00:30:42 +0530

    Add files via upload

---
+++ Smartwatch.js
@@ -0,0 +1,17 @@
+let watch = {
+    title: "Smart Watch",
+    price: 3000,
+    availability: true,
+    colour: ["Black", "Blue"],
+    seller: {
+        name: "ABC Store",
+        address: "Hyderabad"
+    }
+};
+
+console.log("Title:", watch.title);
+console.log("Price:", watch.price);
+console.log("Availability:", watch.availability);
+console.log("Colours:", watch.colour);
+console.log("Seller Name:", watch.seller.name);
+console.log("Seller Address:", watch.seller.address);
